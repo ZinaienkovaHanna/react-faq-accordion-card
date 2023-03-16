fc39b7a0fd0e74595713318394ec8f2853ed17f0
Hanna <zinaienkova.anna@gmail.com>
2023-03-16 14:48:22 +0200

    change code

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -4,27 +4,29 @@ import Tabs from './Tabs';
 const Faq = () => {
   return (
     <section className={styles.bgContainer}>
-      <div className={styles.faqContainer}>
-        <div>
-          <img
-            src="/images/illustration-woman-online-mobile.svg"
-            alt="woman"
-            className={styles.imgWoman}
-          />
-          <img
-            src="/images/illustration-woman-online-desktop.svg"
-            alt="woman"
-            className={styles.imgWoman}
-          />
-          <img
-            src="/images/illustration-box-desktop.svg"
-            alt="box"
-            className={styles.imgBox}
-          />
-        </div>
-        <div className={styles.tabsContainer}>
-          <h2>FAQ</h2>
-          <Tabs />
+      <div className={styles.imgBoxContainer}>
+        <img
+          src="/images/illustration-box-desktop.svg"
+          alt="box"
+          className={styles.imgBox}
+        />
+        <div className={styles.faqContainer}>
+          <div>
+            <img
+              src="/images/illustration-woman-online-mobile.svg"
+              alt="woman"
+              className={styles.imgWoman}
+            />
+            <img
+              src="/images/illustration-woman-online-desktop.svg"
+              alt="woman"
+              className={styles.imgWoman}
+            />
+          </div>
+          <div className={styles.tabsContainer}>
+            <h2>FAQ</h2>
+            <Tabs />
+          </div>
         </div>
       </div>
     </section>
